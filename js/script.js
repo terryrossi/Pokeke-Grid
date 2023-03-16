7ee891cc35c2d3b70fa4e08d6c0370926ebac264
Terry-Rossi <terryrossi1@gmail.com>
2023-03-15 20:15:35 -0700

    Update script.js
    Added function loadDetails (fetch API for more details on each pokemon clicked on)

--- js/script.js
+++ js/script.js
@@ -8,71 +8,10 @@ const pokemonRepository = (function () {
 	// let pokemon = {};
 	let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
 
-	// pokemon = {
-	// 	name: "Bulbasaur",
-	// 	height: 0.7,
-	// 	types: ["grass", "poison"],
-	// 	color: "green",
-	// };
-	// pokemonList.push(pokemon);
-
-	// pokemon = {
-	// 	name: "Ivysaur",
-	// 	height: 1,
-	// 	types: ["grass", "poison"],
-	// 	color: "green",
-	// };
-	// pokemonList.push(pokemon);
-
-	// pokemon = {
-	// 	name: "Venusaur",
-	// 	height: 2,
-	// 	types: ["grass", "poison"],
-	// 	color: "green",
-	// };
-	// pokemonList.push(pokemon);
-
-	// pokemon = {
-	// 	name: "Charmander",
-	// 	height: 0.6,
-	// 	types: ["fire"],
-	// 	color: "red",
-	// };
-	// pokemonList.push(pokemon);
-	///////////////////////////////////////////////////////////////
 	// FUNCTIONS...
 
 	function add(pokemon) {
-		// Check if argument received is NOT of type "Object"
-		// if (typeof pokemon !== "object") {
-		// 	console.log(`The argument passed to the function "add" is of type ${typeof pokemon}`);
-		// 	console.log(
-		// 		`The argument passed to the function "add" MUST be an object
-		//     with the following keys: ${Object.keys(pokemonRepository.getAll()[0])}.`
-		// 	);
-		// 	console.log(
-		// 		`The New Object has NOT been added to the list. Please correct the Error and Resubmit...`
-		// 	);
-		// } else {
-		// find Valid keys from first ogject
-		// console.log(pokemonRepository.getAll()[0]);
-		// const validPokemonKeys = Object.keys(pokemonRepository.getAll()[0]);
-
-		// Compare keys of argument received with valid keys
-		// if (Object.keys(pokemon).toString() !== validPokemonKeys.toString()) {
-		// 	console.log(`INVALID KEYS of the received Argument: `);
-		// 	console.log(`${Object.keys(pokemon)} `);
-
-		// 	console.log(`VALID KEYS are: `);
-		// 	console.log(`${Object.keys(pokemonRepository.getAll()[0])}.`);
-		// 	console.log(
-		// 		`The New Object has NOT been added to the list. Please correct the Error and Resubmit...`
-		// 	);
-		// } else {
-		// If Keys are Valid, Push pokemon to the pokemonRepository
 		pokemonList.push(pokemon);
-		// }
-		// }
 	}
 	///////////////////////////////////////////////
 	function getAll() {
@@ -92,6 +31,23 @@ const pokemonRepository = (function () {
 					};
 					add(pokemon);
 				});
+			})
+			.catch(function (e) {
+				console.error(e);
+			});
+	}
+	////////////////////////////////////////////////
+	function loadDetails(pokemon) {
+		let url = pokemon.detailsUrl;
+		return fetch(url)
+			.then(function (response) {
+				return response.json();
+			})
+			.then(function (pokemonDetails) {
+				// Pokemon details received from API
+				pokemon.imageUrl = pokemonDetails.sprites.other.home.front_default;
+				pokemon.height = pokemonDetails.height;
+				pokemon.types = pokemonDetails.types;
 			})
 			.catch(function (e) {
 				console.error(e);
@@ -131,6 +87,9 @@ const pokemonRepository = (function () {
 	///////////////////////////////////////////////////////////////
 	function showDetails(pokemon) {
 		console.log(pokemon);
+		loadDetails(pokemon).then(function () {
+			console.log(pokemon);
+		});
 	}
 	///////////////////////////////////////////////////////////////
 	function search(searchName) {
@@ -156,6 +115,7 @@ const pokemonRepository = (function () {
 		addListItem,
 		search,
 		showHTML,
+		loadDetails,
 	};
 })();
 //
@@ -165,21 +125,10 @@ const pokemonRepository = (function () {
 
 /////////////////////////////////// PROGRAM FLOW... ///////////////////////////////
 //
-// Add a new Pokemon
-// const newPokemon = {
-// 	name: "Tutu",
-// 	height: 100,
-// 	types: ["car", "truck"],
-// 	color: "purple",
-// };
-// pokemonRepository.add(newPokemon);
 //
 //  FETCH ALL POKEMONS FROM API then SHOW ALL POKEMONS
 pokemonRepository.loadPokeListFromApi().then(function () {
-	pokemonRepository.showHTML(pokemonRepository.getAll());
+	pokemonRepository.getAll().forEach(function (pokemon) {
+		pokemonRepository.addListItem(pokemon);
+	});
 });
-
-// SHOW ONLY SEARCHED POKEMON
-// const pokemonSearchName = "VenUsaur";
-// const resultOfSearch = pokemonRepository.search(pokemonSearchName);
-// pokemonRepository.showHTML(resultOfSearch);
